8dfcd18a6ef64dcfaf2db274e8b4116af59b29c1
kanopo <dmitri.ollari@protonmail.com>
2023-05-19 15:43:02 +0200

    refixed undefined jsx element

--- dashboard/components/LoginButton.tsx
+++ dashboard/components/LoginButton.tsx
@@ -4,18 +4,12 @@ const LoginButton = () => {
   const { data: session, status } = useSession()
 
   if (status === "loading") {
-    return
-    <div className="w-[50vw] flex justify-end items-center">
-      <p>Loading...</p>;
-    </div>
-
-  } else if (status === "unauthenticated") {
     return (
       <div className="w-[50vw] flex justify-end items-center">
-        {/* <p>Not signed in</p> */}
-        <button className="mr-10 border-2 p-2 rounded-md bg-slate-500" onClick={() => signIn()}>Sign in</button>
+        <p>Loading...</p>;
       </div>
     )
+
   } else if (status === "authenticated") {
     return (
       <div className="w-[50vw] flex justify-end items-center">
@@ -24,6 +18,14 @@ const LoginButton = () => {
           session.user?.image && <img src={session?.user?.image} alt="" className="w-[3vw] rounded-full mr-10" />
         }
         <button className="mr-10 border-2 p-2 rounded-md bg-slate-500" onClick={() => signOut()}>Sign out</button>
+      </div>
+    )
+  }
+  else {
+    {/** (status === "unauthenticated")  */ }
+    return (
+      <div className="w-[50vw] flex justify-end items-center">
+        <button className="mr-10 border-2 p-2 rounded-md bg-slate-500" onClick={() => signIn()}>Sign in</button>
       </div>
     )
   }
