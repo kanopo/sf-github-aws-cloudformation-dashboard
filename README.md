ee289e2e160c08aa0a95bc4d330773ac7b3761db
kanopo <dmitri.ollari@protonmail.com>
2023-05-22 18:10:13 +0200

    readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# AWS dashboard
+
+## Description
+Dashboard to deploy github apps on aws cloudformation.
+
+## TODO
+- [  ] fare todo list
--- dashboard/app/api/repos/route.ts
+++ dashboard/app/api/repos/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: Request) {
         auth: process.env.GITHUB_TOKEN,
     });
     const response = await octokit.repos.listForAuthenticatedUser({
-        per_page: 10,
+        per_page: 100,
     });
 
 
