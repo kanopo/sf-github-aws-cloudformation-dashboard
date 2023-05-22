7389f489ed6a0a30c2d660e5cee731e3779a6a7b
kanopo <dmitri.ollari@protonmail.com>
2023-05-22 12:15:11 +0200

    main page now is dynamic

--- dashboard/app/page.tsx
+++ dashboard/app/page.tsx
@@ -1,13 +1,41 @@
 import { FC } from "react";
+import { CloudFormationClient, ListStacksCommand, StackSummary } from "@aws-sdk/client-cloudformation";
+const github = require('octonode');
 
+export const dynamic = 'force-dynamic';
 interface Props {
 }
 
+const getRepos = async () => {
+
+  const client = github.client(process.env.GITHUB_TOKEN);
+  const ghme = client.me();
+  const response = await ghme.reposAsync();
+  return response[0];
+}
+
+const getStacks = async () => {
+
+  const client = new CloudFormationClient({
+    region: process.env.AWS_REGION || 'eu-north-1',
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY || '',
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ''
+    }
+  });
+  const response = await client.send(new ListStacksCommand({}));
+  const stacks = response.StackSummaries;
+  if (!stacks) {
+    return [];
+  }
+  const activestacks: StackSummary[] = stacks.filter(stack => stack.StackStatus !== 'DELETE_COMPLETE');
+  return activestacks;
+}
+
 const Page = async ({ }) => {
-  const responceRepos = await fetch(process.env.URL + '/api/repos')
-  const responceStacks = await fetch(process.env.URL + '/api/aws-stacks')
-  const dataRepos = await responceRepos.json()
-  const dataStacks = await responceStacks.json()
+  const dataRepos = await getRepos()
+  const dataStacks = await getStacks()
+
   return (
     <div className="flex flex-col">
       <div className="flex w-full">
